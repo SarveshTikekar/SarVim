41c57e6d133b90c3fdb7fecaa5d29315c966c935
T-SarvesH <sarvesh1.tikekar@gmail.com>
2025-12-12 13:16:19 +0530

    Started with the plugins + lazy vim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,14 @@
+-- Main Entry to our Ricing setup
 
 require("sarveshtikekar.landing_page.land_page").show()
 require("sarveshtikekar.remaps")
+
+
+-- Entry point for lazy plugins
+
+vim.opt.rtp:prepend("/home/sarvesh/.local/share/nvim/lazy/lazy.nvim")
+
+require("lazy").setup("sarveshtikekar.plugins", {
+
+	ui = {open = "none"}
+})
